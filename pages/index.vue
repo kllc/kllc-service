2b91b2e181867992cfe7c822380e8bfb618ce9b4
K <kijima@kllc.jp>
2021-12-13 16:42:08 +0900

    211213

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <el-container>
     <Header />
     <el-row class="container">
-      <el-col :xs="24" :sm="20" :md="20" :lg="18" :xl="16" class="content-wrap">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
           <el-row class="row-1st">
             <el-col :span="20" class="col-title">
-              <h1 class="box-title">QAChat</h1> 
+              <h1 style="border-bottom:4px solid #fd9534" class="box-title">QAChat</h1>
             </el-col>
             <el-col :span="4" class="col-logo">
               <img class="img-logo" src="https://www.ikazuchi.biz/marketplace/kanri/image.php?file=service_detail.19.1.jpg" alt="">
@@ -38,10 +38,46 @@
     </el-row>
 
     <el-row class="container">
-      <el-col :xs="24" :sm="20" :md="20" :lg="18" :xl="16" class="content-wrap">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
           <el-row class="row-1st">
             <el-col :span="20" class="col-title">
-              <h1 class="box-title">ミイダス</h1> 
+              <h1 style="border-bottom:4px solid #388bf3" class="box-title">SAPPRO NEST</h1>
+            </el-col>
+            <el-col :span="4" class="col-logo">
+              <img class="img-logo" src="https://sapporo.nest.co.jp/wp-content/themes/sapporonest/images/header_logo.svg" alt="">
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <img class="col-image" src="https://sapporo.nest.co.jp/wp-content/themes/sapporonest/images/fac_img01.jpg" alt="">
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
+              <h2 class="ttl">経験と価値の交換所「SAPPRO NEST」</h2>
+              <p>
+                オフィスという形に捉われない自由度の高さをコンセプトに、あなたの起業、副業をサポートいたします。<br />デスクワーク以外の業態にも対応できるプランもご用意しております。<br /><br />
+                起業家向けの様々な支援サービスのほか、事業内容により、東京の新規お取引先・顧客のご紹介も可能です。<br /> また、CoWorking(共有)スペースもございますので、交流会などビジネス利用以外での使用も可能です。 <br />我々は、チャレンジするすべての人の『NEST』を目指しています。
+              </p>
+            </el-col>
+          </el-row>
+          <el-row class="row-button">
+            <!-- <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-button">
+                <el-row> -->
+                  <el-col class="col-button">
+                    <div class="btn-copy">お問い合わせはこちら</div>
+                    <a href="https://sapporo.nest.co.jp/" target="_blank" class="btn btn--orange"><i class="fas fa-phone-alt fa-position-left"></i>公式ページ</a>
+                    <!-- <el-button class="button" @click="urlopen('https://kllc.jp')" >デモページ</el-button>&nbsp;&nbsp;QAChatデモページへ -->
+                  </el-col>
+                <!-- </el-row>
+            </el-col> -->
+          </el-row>
+      </el-col>
+    </el-row>
+
+    <el-row class="container">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
+          <el-row class="row-1st">
+            <el-col :span="20" class="col-title">
+              <h1 style="border-bottom:4px solid #ffbb2a" class="box-title">ミイダス</h1>
             </el-col>
             <el-col :span="4" class="col-logo">
               <img class="img-logo" src="https://cdn2.hubspot.net/hub/5179987/hubfs/LP-dr-vol1/images/illust-p4.png?width=780&height=564&name=illust-p4.png" alt="">
@@ -49,13 +85,15 @@
           </el-row>
           <el-row>
             <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
-              <img class="col-image" src="https://cdn2.hubspot.net/hub/5179987/hubfs/LP-dr-vol1/images/illust-p1.png?width=460&height=452&name=illust-p1.png" alt="">
+              <img class="col-image" src="https://cdn2.hubspot.net/hub/5179987/hubfs/LP-dr-vol1/images/graph-p1.jpg?width=880&height=552&name=graph-p1.jpg" alt="">
             </el-col>
             <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
-              <h2 class="ttl">追加料金一切なしで、年間採用し放題の採用支援サービス</h2>
+              <h2 class="ttl">追加料金なし！採用し放題の採用支援サービス</h2>
               <p>
-                ああああああああああああああああああああああああああああああああああああああああああ。<br /><br />
-                ああああああああああああああああああああああああああああああああああああああああああ。
+                追加料金一切なしで、年間採用し放題の採用支援サービスです。
+                定額制だから採用すればするほどオトクなプラン。<br /><br />
+                37万人以上が登録する豊富なデータベースから優秀な人材を取り放題！<br />
+都心のみならず地方企業の採用にも絶大な成果！
               </p>
             </el-col>
           </el-row>
@@ -74,10 +112,10 @@
     </el-row>
 
     <el-row class="container">
-      <el-col :xs="24" :sm="20" :md="20" :lg="18" :xl="16" class="content-wrap">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
           <el-row class="row-1st">
             <el-col :span="20" class="col-title">
-              <h1 class="box-title">コピー機革命</h1> 
+              <h1 style="border-bottom:4px solid #f55e61" class="box-title">コピー機革命</h1>
             </el-col>
             <el-col :span="4" class="col-logo">
               <img class="img-logo" src="https://cdn2.hubspot.net/hubfs/5179987/%E3%82%B3%E3%83%94%E3%83%BC%E3%82%AD%E9%9D%A9%E5%91%BD%EF%BC%88%E3%83%87%E3%83%95%E3%82%A9%E3%83%AB%E3%83%88%EF%BC%89/c_ph05.jpg" alt="">
@@ -90,8 +128,8 @@
             <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
               <h2 class="ttl">定価100万円を超える複合機が、4000円/月（印刷代4000円込）でレンタル可能。</h2>
               <p>
-                ああああああああああああああああああああああああああああああああああああああああああ。<br /><br />
-                ああああああああああああああああああああああああああああああああああああああああああ。
+                業務用複合機の０円レンタルサービスをご存知ですか？月額リース費用が無料で、しかも格安の印刷コストで利用可能です！<br /><br />
+                全国対応開始。まずは設置費用などのご確認を！リース料無料で印刷費用のみでご利用いただけます。起業時、サブ機などのご利用が増えています。
               </p>
             </el-col>
           </el-row>
@@ -110,10 +148,10 @@
     </el-row>
 
     <el-row class="container">
-      <el-col :xs="24" :sm="20" :md="20" :lg="18" :xl="16" class="content-wrap">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
           <el-row class="row-1st">
             <el-col :span="20" class="col-title">
-              <h1 class="box-title">MG研修（無料体験）</h1> 
+              <h1 style="border-bottom:4px solid #008953" class="box-title">MG研修（無料体験）</h1>
             </el-col>
             <el-col :span="4" class="col-logo">
               <img class="img-logo" src="https://cdn2.hubspot.net/hub/5179987/hubfs/mg-online-logo2.jpg?width=185&name=mg-online-logo2.jpg" alt="">
@@ -121,13 +159,14 @@
           </el-row>
           <el-row>
             <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
-              <img class="col-image" src="https://cdn2.hubspot.net/hub/5179987/hubfs/RELO_CLUB%E7%94%BB%E5%83%8F/illust-04-mg.png?width=200&height=214&name=illust-04-mg.png" alt="">
+              <img class="col-image" src="https://cdn2.hubspot.net/hubfs/5179987/mg_boad.jpg" alt="">
             </el-col>
             <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
               <h2 class="ttl">『あの』MG研修が、無料で体験可能！！</h2>
               <p>
-                ああああああああああああああああああああああああああああああああああああああああああ。<br /><br />
-                ああああああああああああああああああああああああああああああああああああああああああ。
+マネジメントＭＧ（ＭＧ研修）は、約４０年前にソニーが開発した経営者育成研修です。<br />自社のエンジニアにマネジメントのノウハウを短期間で、”面白く”学んでもらうにはどうすればいいか？を考え抜き、創り上げたものです。<br /><br />
+元々、ソニーの社内用研修でしたが、他社でも活用されるようになり、米国や韓国を始め近年では中国や東南アジアなど世界中で実施されています。<br />
+ＭＧ研修の最大の特徴は、「ゲームを通じて」「楽しく」「経営体験」ができることです。
               </p>
             </el-col>
           </el-row>
@@ -145,69 +184,112 @@
       </el-col>
     </el-row>
 
-
-■MG研修（無料体験）：
-たかがゲームで終わらないすごさがここにある。。。
-『あの』MG研修が、無料で体験可能！！
-  ■MG研修（無料体験）：
-> https://writeup-5179987.hs-sites.com/mg-tpl-02-165-18
-
-
-■Jシステム（補助金・助成金自動診断システム）：
-約１分で、受給可能な補助金、助成金の総額がわかります。
-> ■Jシステム：
-> https://writeup-5179987.hs-sites.com/jdnjsys-63
-
-
-■Jコンサル：
-格安で、その年に活用できる助成金をご紹介し、
-受給するまで最大受給額を目指してサポート致します。
-※返金保障制度あり。
-> ■Jコンサル：
-> https://writeup-5179987.hs-sites.com/jcon-jdn-398
-
-■WEBコンサル：
-制作・分析・集客まで、ワンストップでサポート
-> ■WEBコンサル：
-> https://writeup-5179987.hs-sites.com/jdn-webconsulting-24
-
-
-
-
-
-
-
-
-    <el-row :gutter="0" class="container">
-      <el-col :xs="24" :sm="20" :md="16" :lg="16" :xl="12" class="content-wrap">
-        abc
+    <el-row class="container">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
+          <el-row class="row-1st">
+            <el-col :span="20" class="col-title">
+              <h1 style="border-bottom:4px solid #c3343b" class="box-title">Jシステム</h1>
+            </el-col>
+            <el-col :span="4" class="col-logo">
+              <img class="img-logo" src="https://www.jengine.jp/assets/images/_top-illust-ph03.jpg" alt="">
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <img class="col-image" src="https://www.jmatch.jp/assets/images/about_ph02.jpg" alt="">
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
+              <h2 class="ttl">約１分で、受給可能な補助金、助成金の総額がわかります。</h2>
+              <p>
+                受給「最低金額」と「最高金額」が自動診断されます。常に最新情報に自動更新されるため、非常に「正確な」金額です。<br /><br />
+                独自のヒヤリング項目を最大１０種類まで設定可能。更には関連する自社サービスを自動表示することができます。本業売上アップのための重要な機能です。<br /><br />
+                新しい助成金ほど申請手順は不明確なところも多く手間がかかります。本システムではその場で申請依頼まで実施できオンライン対応のみで申請まで進むことが可能です。
+              </p>
+            </el-col>
+          </el-row>
+          <el-row class="row-button">
+            <!-- <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-button">
+                <el-row> -->
+                  <el-col class="col-button">
+                    <div class="btn-copy">お問い合わせ資料請求はこちら</div>
+                    <a href="https://writeup-5179987.hs-sites.com/jdnjsys-63" target="_blank" class="btn btn--orange"><i class="fas fa-phone-alt fa-position-left"></i>詳細ページ</a>
+                  </el-col>
+                <!-- </el-row>
+            </el-col> -->
+          </el-row>
       </el-col>
     </el-row>
-        <div class="content-wrap">
-          <el-rate v-model="value" :colors="colors" :max="10"></el-rate>
-        </div>
-        <div class="content-wrap">
-          <el-progress :text-inside="true" :stroke-width="26" :percentage="70"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="24" :percentage="100" status="success"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="22" :percentage="80" status="warning"></el-progress>
-          <el-progress :text-inside="true" :stroke-width="20" :percentage="50" status="exception"></el-progress>
-        </div>
-        <div class="content-wrap">
-          <el-button type="text" @click="dialogVisible = true">ダイアログを開きます</el-button>
 
-          <el-dialog
-            title="ダイアログタイトル"
-            :visible.sync="dialogVisible"
-            width="30%"
-            :before-close="handleClose"
-          >
-            <span>ダイアログです。</span>
-            <span slot="footer" class="dialog-footer">
-              <el-button @click="dialogVisible = false">キャンセル</el-button>
-              <el-button type="primary" @click="dialogVisible = false">OK</el-button>
-            </span>
-          </el-dialog>
-        </div>
+    <el-row class="container">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
+          <el-row class="row-1st">
+            <el-col :span="20" class="col-title">
+              <h1 style="border-bottom:4px solid #ffe584" class="box-title">Jコンサル</h1>
+            </el-col>
+            <el-col :span="4" class="col-logo">
+              <img class="img-logo" src="https://www.jengine.jp/assets/images/_top-illust-ph02.jpg" alt="">
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <img class="col-image" src="https://f.hubspotusercontent30.net/hub/5179987/hubfs/Rich%20Text%20Pasting/unknown-1593135460161-1.png?width=600&height=312&name=unknown-1593135460161-1.png" alt="">
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
+              <h2 class="ttl">最大受給額を目指してサポート致します</h2>
+              <p>
+                格安で、その年に活用できる助成金をご紹介し、受給するまでサポートいたします。<br/>※返金保障制度あり。<br/><br/>
+                まずは、無料で「助成金の無料診断」を依頼することができます。<br />後日、弊社コンサルタントからお電話でご連絡させていただき、「助成金の受給見込額」をお伝えいたします。<br /><br />
+                正式にお申込みを頂くまでは、費用は一切発生しません。<br />「真面目に申請を考えてみたい」とお考えの方は、お気軽にご相談ください。
+              </p>
+            </el-col>
+          </el-row>
+          <el-row class="row-button">
+            <!-- <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-button">
+                <el-row> -->
+                  <el-col class="col-button">
+                    <div class="btn-copy">お問い合わせ資料請求はこちら</div>
+                    <a href="https://writeup-5179987.hs-sites.com/jcon-jdn-398" target="_blank" class="btn btn--orange"><i class="fas fa-phone-alt fa-position-left"></i>詳細ページ</a>
+                  </el-col>
+                <!-- </el-row>
+            </el-col> -->
+          </el-row>
+      </el-col>
+    </el-row>
+
+    <el-row class="container">
+      <el-col :xs="24" :sm="20" :md="18" :lg="16" :xl="14" class="content-wrap">
+          <el-row class="row-1st">
+            <el-col :span="20" class="col-title">
+              <h1 style="border-bottom:4px solid #077fdd" class="box-title">WEBコンサルタント</h1>
+            </el-col>
+            <el-col :span="4" class="col-logo">
+              <img class="img-logo" src="https://f.hubspotusercontent30.net/hubfs/5179987/WEB%E3%82%B3%E3%83%B3LP/top_intro_img01.png" alt="">
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+              <img class="col-image" src="https://f.hubspotusercontent30.net/hubfs/5179987/WEB%E3%82%B3%E3%83%B3LP/top_intro_figure01.png" alt="">
+            </el-col>
+            <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-lead">
+              <h2 class="ttl">制作・分析・集客まで、ワンストップでサポート</h2>
+              <p>
+                販売実績130社以上！<br />中小企業向けの安価なホームページ制作から競合分析、広告による集客まで全てサポートいたします。<br /><br />公的支援制度コンサルによるコスト削減提案も可能です。
+              </p>
+            </el-col>
+          </el-row>
+          <el-row class="row-button">
+            <!-- <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="col-button">
+                <el-row> -->
+                  <el-col class="col-button">
+                    <div class="btn-copy">お問い合わせ資料請求はこちら</div>
+                    <a href="https://writeup-5179987.hs-sites.com/jdn-webconsulting-24" target="_blank" class="btn btn--orange"><i class="fas fa-phone-alt fa-position-left"></i>詳細ページ</a>
+                  </el-col>
+                <!-- </el-row>
+            </el-col> -->
+          </el-row>
+      </el-col>
+    </el-row>
+
 
     <el-footer>: container for footers.
     </el-footer>
@@ -289,6 +371,7 @@ body {
 
   .img-logo{
     height: 80px;
+    max-width: 140px;
   }
 
   .row-button {
@@ -349,19 +432,22 @@ body {
 
 .box-title {
   position: relative;
-	margin: 5px 0 0 0;
-	padding: 8px 27px;
-  color: #fff;
-  border-radius: 10px;
+	/* margin: 5px 0 0 0; */
+	/* padding: 8px 27px; */
+	margin: 0;
+	padding: 0;
+  color: #000;
+  /* border-radius: 10px; */
+  font-size: 2em;
   /* background-image: -webkit-gradient(linear, left top, right top, from(#308bbe), to(#4eb4ad));
   background-image: -webkit-linear-gradient(left, #308bbe 0%, #4eb4ad 100%);
   background-image: linear-gradient(to right, #308bbe 0%, #4eb4ad 100%); */
-  background-image: -webkit-gradient(linear, left top, right top, from(#0d6ea3), to(#fff));
+  /* background-image: -webkit-gradient(linear, left top, right top, from(#0d6ea3), to(#fff));
   background-image: -webkit-linear-gradient(left, #0d6ea3 0%, #fff 100%);
-  background-image: linear-gradient(to right, #0d6ea3 0%, #fff 100%);
+  background-image: linear-gradient(to right, #0d6ea3 0%, #fff 100%); */
 }
 
-.box-title:after {
+/* .box-title:after {
   position: absolute;
   bottom: -9px;
   left: 1em;
@@ -371,7 +457,7 @@ body {
   border-width: 10px 10px 0 10px;
   border-style: solid;
   border-color: #0d6ea3 transparent transparent transparent;
-}
+} */
 
 /*まずはお決まりのボックスサイズ算出をborer-boxに */
 /* button */
@@ -406,7 +492,7 @@ body {
   .btn,
   a.btn,
   button.btn {
-    font-size: 1.5rem;
+    font-size: 3vmin;
     font-weight: 700;
     line-height: 1.5;
     position: relative;
