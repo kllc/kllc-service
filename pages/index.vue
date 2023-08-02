f8b0b6e2353943ddbc050f17ddd561bdc841e8fc
K <kijima@kllc.jp>
2023-08-02 14:27:35 +0900

    pwa

--- pages/index.vue
+++ pages/index.vue
@@ -537,7 +537,10 @@
       type="application/javascript"
       src="https://livelp.net/1.2.6/livelp.js"
     ></script>
-
+    <a
+      href="https://livelp.net/1.2.6/iframe.html?key=fa8461525e2c881c29622abd622147ae648d2a1181b4959d602ae5af91977721"
+      >リンク</a
+    >
     <el-footer class="footer">©2021KLLC </el-footer>
   </el-container>
 </template>
